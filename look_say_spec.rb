60b9f509c94a1d99704fc5cb0b36750c32267a21
Aleksei Matskevich <aleksei_mackevich@outlook.com>
2018-11-12 22:00:14 +0300

    add test for #rewind, #value_at, chang #next

--- look_say_spec.rb
+++ look_say_spec.rb
@@ -5,23 +5,29 @@ describe LookSay do
   subject(:sequence) { LookSay.new }
 
   let(:reference) do
-    [1,
-     11,
-     21,
-     1211,
-     111_221,
-     312_211,
-     13_112_221,
-     1_113_213_211,
-     31_131_211_131_221,
-     13_211_311_123_113_112_211]
+    [1, 11, 21, 1211, 111_221, 312_211, 13_112_221, 1_113_213_211,
+     31_131_211_131_221, 13_211_311_123_113_112_211]
   end
 
   describe '#next' do
-    it 'return next value' do
+    it 'return next item' do
+      5.times do |key|
+        expect(sequence.next).to eq(reference[key])
+      end
+    end
+  end
+
+  describe 'value_at' do
+    it 'return' do
+      expect(sequence.value_at(3)).to eq(reference[3])
+    end
+  end
+
+  describe '#rewind' do
+    it 'Rewinds the enumeration sequence to the beginning' do
+      sequence.next
+      sequence.rewind
       expect(sequence.next).to eq(1)
-      expect(sequence.next).to eq(11)
-      expect(sequence.next).to eq(21)
     end
   end
 
